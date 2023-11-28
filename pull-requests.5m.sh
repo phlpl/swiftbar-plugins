130e12ffee4cc3cd6fad05430df4b725be8e0428
phlpl <philip.eel@gmail.com>
2023-11-28 13:46:40 +0000

    initial commit

---
+++ pull-requests.5m.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# <bitbar.title>Pull requests plugin</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>phlpl</bitbar.author>
+# <bitbar.author.github>phlpl</bitbar.author.github>
+# <bitbar.desc>Shows prs for 1 repo.  Node version in bash, for ref: https://github.com/matryer/xbar-plugins/blob/main/Dev/GitHub/pull-requests.5m.js</bitbar.desc>
+# <bitbar.dependencies>brew install jq</bitbar.dependencies>
+# <bitbar.abouturl>https://github.com/phlpl</bitbar.abouturl>
+#
+# References:
+# https://github.com/matryer/xbar-plugins/blob/main/Dev/GitHub/pull-requests.5m.js
+# https://docs.github.com/en/rest/overview/authenticating-to-the-rest-api?apiVersion=2022-11-28
+# https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#list-pull-requests
+# https://jqlang.github.io/jq/
+#
+# Dependencies:
+# brew install jq
+# 
+# Todos:
+# Allow for more than 1 repo
+# Check timestamp to show 'new'
+
+repo='OWNER/REPO'
+token='FINE-GRAINED-TOKEN'
+
+results=$(curl -s -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $token" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$repo/pulls)
+
+count=$(echo $results | jq length)
+
+echo "pr:$count"
+echo "---"
+
+for (( i=0; i<$count; i++ ))
+do
+    # Extract the required fields for each item
+    number=$(echo $results | jq -r --argjson i $i '.[$i].number')
+    title=$(echo $results | jq -r --argjson i $i '.[$i].title')
+    user_login=$(echo $results | jq -r --argjson i $i '.[$i].user.login')
+    html_url=$(echo $results | jq -r --argjson i $i '.[$i].html_url')
+    echo "#$number $title ($user_login) | href=$html_url"
+done
